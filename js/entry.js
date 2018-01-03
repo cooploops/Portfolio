58ad2039af2a6ff385bec91194fe47ce9624e6f4
TJ <cooploops@gmail.com>
2018-01-03 15:20:12 -0800

    add entry page

---
+++ js/entry.js
@@ -0,0 +1,18 @@
+    const left = $(".left");
+    const right = $(".right");
+    const landing = $(".landing");
+    
+    left.hover(function(){
+        landing.addClass("hover-left")},
+        function(){
+        landing.removeClass("hover-left")
+        }
+    );
+    
+    right.hover(function(){
+        landing.addClass("hover-right")}, 
+        function(){
+            landing.removeClass("hover-right")
+        }
+    );
+
